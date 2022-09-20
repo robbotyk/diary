6273ad7503e23b52f411e9b63fbb6487d8991fc7
robbotyk <09014198535a@gmail.com>
2022-09-20 14:11:05 +0900

    edit画面のバリデーションの追加

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -35,6 +35,7 @@
                     </div>
                     <div class='weather'>
                         <h2>天気 <input type="text" name="diary[weather]" value="{{$input->weather}}"></h2>
+                        <p class="weather__errors" style="color:red">{{ $errors->first('diary.weather') }}</p>
                         <h2>名前 ユーザー名</h2>
                     </div>
                 </div>
@@ -42,42 +43,52 @@
                     <div><h2>1,2限目</h2></div>
                     <div class='First_class'>
                         <input type="text" name="diary[First_class]" placeholder="教科" value="{{$input->First_class}}"/>
+                        <p class="First_class__errors" style="color:red">{{ $errors->first('diary.First_class')}}</p>
                     </div>
                     <div class='First_content'>
                         <textarea name="diary[First_content]" placeholder="内容">{{$input->First_content}}</textarea>
+                        <p class="First_content__errors" style="color:red">{{ $errors->first('diary.First_content')}}</p>
                     </div>    
                     <div class='First_impression'>
                         <textarea name="diary[First_impression]" placeholder="所感">{{$input->First_impression}}</textarea>
+                        <p class="First_impression__errors" style="color:red">{{ $errors->first('diary.First_impression')}}</p>
                     </div>   
                 </div>
                 <div class='secound'>
                     <div><h2>3,4限目</h2></div>
                     <div class='Secound_class'>
                         <input type="text" name="diary[Secound_class]" placeholder="教科" value="{{$input->Secound_class}}"/>
+                        <p class="Secound_class__errors" style="color:red">{{ $errors->first('diary.Secound_class')}}</p>
                     </div>
                     <div class='Secound_content'>
                         <textarea name="diary[Secound_content]" placeholder="内容">{{$input->Secound_content}}</textarea>
+                        <p class="Secound_content__errors" style="color:red">{{ $errors->first('diary.Secound_content')}}</p>
                     </div>    
                     <div class='Secound_impression'>
                         <textarea name="diary[Secound_impression]" placeholder="所感">{{$input->Secound_impression}}</textarea>
+                        <p class="Secound_impression__errors" style="color:red">{{ $errors->first('diary.Secound_impression')}}</p>
                     </div>   
                 </div>
                 <div class='third'>
                     <div><h2>5,6限目</h2></div>
                     <div class='Third_class'>
                         <input type="text" name="diary[Third_class]" placeholder="教科" value="{{$input->Third_class}}"/>
+                        <p class="Third_class__errors" style="color:red">{{ $errors->first('diary.Third_class')}}</p>
                     </div>
                     <div class='Third_content'>
                         <textarea name="diary[Third_content]" placeholder="内容">{{$input->Third_content}}</textarea>
+                        <p class="Third_content__errors" style="color:red">{{ $errors->first('diary.Third_content')}}</p>
                     </div>    
                     <div class='Third_impression'>
                         <textarea name="diary[Third_impression]" placeholder="所感">{{$input->Third_impression}}</textarea>
+                        <p class="Third_impression__errors" style="color:red">{{ $errors->first('diary.Third_impression')}}</p>
                     </div>  
                 </div>
                 <div class='total'>
                     <div><h2>総合所感</h2></div>
                     <div class='Total_impression'>
                         <textarea name="diary[Total_impression]" placeholder="総合所感">{{$input->Total_impression}}</textarea>
+                        <p class="Total_impression__errors" style="color:red">{{ $errors->first('diary.Total_impression')}}</p>
                     </div>
                 </div>
                 <div class=button>
